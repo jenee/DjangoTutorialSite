5f5acf2a506d632ce77e67a89b51b733e9987a21
Jenee <jenee.hughes@gmail.com>
2013-09-02 12:16:35 -0700

    Made the pub date field collapsable by default
    Now on section: Adding related objects¶

--- mysite/polls/admin.py
+++ mysite/polls/admin.py
@@ -4,7 +4,8 @@ from polls.models import Poll
 class PollAdmin(admin.ModelAdmin):
    fieldsets = [
       (None,               {'fields': ['question']}),
-      ('Date information', {'fields': ['pub_date']}),
+      ('Date information', {'fields': ['pub_date'], 'classes': ['collapse']}),
+      
    ]
 
 admin.site.register(Poll, PollAdmin)
